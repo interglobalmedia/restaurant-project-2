b41207dd757fb95edf8ec52b5f1be1675c8d169b
interglobalmedia <interglobalmedia@gmail.com>
2017-04-07 16:08:32 -0400

    First commit. Basically completed workflow. Now ready to add some styles and js.

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssvars = require('postcss-simple-vars');
+const nested = require('postcss-nested');
+const cssImport = require('postcss-import');
+const mixins = require('postcss-mixins');
+const hexrgba = require('postcss-hexrgba');
+
+gulp.task('styles', () => {
+	return gulp.src('./app/assets/styles/styles.css')
+		.pipe(postcss([cssImport, cssvars, mixins, nested, hexrgba, autoprefixer]))
+		.on('error', function(errorInfo) {
+			console.log((errorInfo.toString()));
+			this.emit('end');
+		})
+		.pipe(gulp.dest('./app/temp/styles'));
+});
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+	entry: {
+		app: './app/assets/scripts/app.js',
+		vendor: './app/assets/scripts/vendor.js'
+	},
+	output: {
+		path: path.resolve(__dirname, 'app/temp/scripts'),
+		filename: '[name].js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['env', 'stage-1']
+					}
+				},
+				exclude: /node_modules/,
+			},
+		]
+	},
+}
